8ac48788cce691b87d49442612343f1d93bc0bf4
Shakhnazar Sailaukan <101112128+Sailaukan@users.noreply.github.com>
2024-07-28 11:50:15 +0500

    added favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@
 import BetaPreview from './BetaPreview/page';
 import MainPage from './components/MainPage';
 import RootLayout from './RootLayout';
+import Head from "next/head";
 
 
 export default function Home() {
   return (
     <RootLayout>
+      <Head>
+        <link rel="icon" href="/motoicon.ico" />
+      </Head>
       <MainPage />
       {/* <BetaPreview/> */}
     </RootLayout>
