88c3b20e25020b945569c36ca6ac23884715f79f
Shakhnazar Sailaukan <101112128+Sailaukan@users.noreply.github.com>
2024-11-20 21:52:27 +0500

    fix: gateway

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: Request) {
         const chatCompletion = await openai.chat.completions.create({
             model: "o1-preview",
             messages: messages,
-            max_tokens: 10000,
+            max_tokens: 5000,
         });
 
         return NextResponse.json({
